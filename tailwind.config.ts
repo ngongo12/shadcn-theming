a68134170c72586227f7de8a3277f84fff5f4c46
Ngô Quốc Vinh <71309432+ngongo12@users.noreply.github.com>
2024-06-21 08:27:32 +0700

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-import { shadcnPreset } from "@/shared-core/lib/shadcn-preset"
 import type { Config } from "tailwindcss"
+
+import { shadcnPreset } from "./shared-core/lib/shadcn-preset"
 
 const config: Config = {
   presets: [shadcnPreset],
